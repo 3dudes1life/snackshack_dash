854f61eb2304e1df350d3c2cac88aa7f36178351
3dudes1life <3dudes1life@gmail.com>
2026-07-08 11:10:55 -0700

    Update config.js

--- data/config.js
+++ data/config.js
@@ -1,6 +1,5 @@
 // Optional live backend.
 // Paste your deployed Google Apps Script Web App URL here when ready.
 // Leave blank to use the sample data in data/sample-data.js.
-window.SNACKSHACK_API_URL = "";
-// Example:
+window.SNACKSHACK_API_URL = "https://script.google.com/macros/s/AKfycbx2t2eP2ZTpcwd6bp3L3bJJBqBdxitN2Bs_2kvGxW0h2_bljZd5_jU3wZQ3cPFjURxS0g/exec";// Example:
 // window.SNACKSHACK_API_URL = "https://script.google.com/macros/s/AKfycb.../exec";
